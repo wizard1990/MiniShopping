e67d5e1a1baf2dd331e661f97332e7c3ef7cff17
wizard1990 <wizard19900509@gmail.com>
2014-04-11 15:53:52 -0700

    login role

--- src/actions/LoginAction.java
+++ src/actions/LoginAction.java
@@ -9,19 +9,28 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
 	private String name;
-	void setName(String name) {
+	private Integer role;
+	public void setName(String name) {
 		this.name = name;
 	}
-	String getName() {
+	public String getName() {
 		return name;
 	}
 	
 	public String execute() throws Exception{
 		UsertableDAO userDAO = new UsertableDAO();
 		List l = userDAO.findByName(name);
-		if (l.size() > 0) {
+		if (l.size() == 1) {
+			Usertable user = (Usertable) l.get(0);
+			setRole(user.getRole());
 			return SUCCESS;
 		}
 		else return ERROR;
+	}
+	public Integer getRole() {
+		return role;
+	}
+	public void setRole(Integer role) {
+		this.role = role;
 	}
 }
