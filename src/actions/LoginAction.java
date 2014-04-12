06db9397715b7177dbd95c63575fafb7a5fb4dc2
wizard1990 <wizard19900509@gmail.com>
2014-04-12 14:25:12 -0700

    list test

--- src/actions/LoginAction.java
+++ src/actions/LoginAction.java
@@ -14,8 +14,6 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
 	private String name;
-	private Integer role;
-	private List list;
 	
 	public void setName(String name) {
 		this.name = name;
@@ -28,26 +26,14 @@ public class LoginAction extends ActionSupport {
 		UsertableDAO userDAO = new UsertableDAO();
 		List l = userDAO.findByName(name);
 		if (l.size() == 1) {
-			list = userDAO.findAll();
 			Usertable user = (Usertable) l.get(0);
 			String r = user.getRole().toString();
 			HttpServletRequest request = ServletActionContext.getRequest();
+			request.setAttribute("alllist", userDAO.findAll());
 			HttpSession session = request.getSession();
 			session.setAttribute("userrole", r);
 			return SUCCESS;
 		}
 		else return ERROR;
-	}
-	public Integer getRole() {
-		return role;
-	}
-	public void setRole(Integer role) {
-		this.role = role;
-	}
-	public List getList() {
-		return list;
-	}
-	public void setList(List list) {
-		this.list = list;
 	}
 }
