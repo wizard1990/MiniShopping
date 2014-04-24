4d059929b9cd51b4264ffd1880d892fba2e10c5a
wizard1990 <wizard19900509@gmail.com>
2014-04-23 21:42:06 -0700

    set attr

--- src/actions/ListCategoriesAction.java
+++ src/actions/ListCategoriesAction.java
@@ -3,11 +3,8 @@ package actions;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.apache.struts2.ServletActionContext;
 
-import DBModel.Category;
 import DBModel.CategoryDAO;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -19,11 +16,9 @@ public class ListCategoriesAction extends ActionSupport {
 		HttpServletRequest request = ServletActionContext.getRequest();
 		if (l.size() > 0) {
 			request.setAttribute("categories", l);
-			request.setAttribute("isSucc", 1);
 			return SUCCESS;
 		}
 		else {
-			request.setAttribute("isSucc", 0);
 			return "empty";
 		}
 	}
