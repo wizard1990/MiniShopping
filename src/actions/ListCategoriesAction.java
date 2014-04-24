6f01cfd3152d332a55721fe28c757287b8774e44
wizard1990 <wizard19900509@gmail.com>
2014-04-23 17:10:55 -0700

    cate

---
+++ src/actions/ListCategoriesAction.java
@@ -0,0 +1,27 @@
+package actions;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import DBModel.Category;
+import DBModel.CategoryDAO;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ListCategoriesAction extends ActionSupport {
+	public String execute() throws Exception{
+		CategoryDAO cateDAO = new CategoryDAO();
+		List l = cateDAO.findAll();
+		System.out.println(l.size());
+		if (l.size() > 0) {
+			HttpServletRequest request = ServletActionContext.getRequest();
+			request.setAttribute("categories", l);
+			return SUCCESS;
+		}
+		else return "empty";
+	}
+}
