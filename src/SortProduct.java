d205fc8366e4afec72c4b5da431ba9741f97398e
yingyanhua <hippocrene814@gmail.com>
2014-04-27 10:37:34 -0700

    change sort function

---
+++ src/SortProduct.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+import DBModel.Product;
+
+public class SortProduct implements Comparator<Product>{
+	@Override
+	public int compare(Product arg0, Product arg1) {
+		if (arg0.getId() > arg1.getId()) {
+			return 1;
+		}
+		else return -1;
+	}
+}
